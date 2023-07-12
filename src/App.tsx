a2839a785e2e354ce2230f53c3aea54cd1a0082a
Victor Porton <porton.victor@gmail.com>
2023-07-12 23:21:37 +0300

    attempted setup with Webpack

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Header } from './components/header';
+// import { Main } from './components/main';
+// import { Footer } from './components/footer';
+// import './styles.css';
+export const App = () => {
+ return (
+  <>
+   {/*<Header />
+   <Main />
+ <Footer />*/}
+    <h1>Test</h1>
+  </>
+ );
+};
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: 
+           {
+              path: path.resolve(__dirname, 'dist'),
+              filename:'main.js',
+           },
+    plugins: [new HtmlWebpackPlugin({ template: './src/index.tsx' })],
+};
